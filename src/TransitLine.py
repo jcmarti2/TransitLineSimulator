71b59d025409dedefaa3eb0849a3a45071ccea0c
Juan Carlos Martinez <jcmarti2@illinois.edu>
2016-07-22 11:46:21 -0500

    Status: Discussed code ...

--- src/TransitLine.py
+++ src/TransitLine.py
@@ -35,8 +35,6 @@ class TransitLine:
 
         # stops and buses list
         self._max_clk = None
-        self._stops = None
-        self._buses = None
 
     def simulate(self, rep_id, max_clk, save_bus=False, save_stop=False):
         """
@@ -83,12 +81,12 @@ class TransitLine:
 
             # initialize simulation
             self._initialize()
-            event_type = 1
+            event_type = 1  # initialize is event_type 1
             stop_id, stop_pax = '', ''
             bus_id, bus_type, bus_pax = '', '', ''
             line = '{0},{1:.2f},{2},{3},{4},{5},{6},{7}\n'.format(ent, clk, event_type, stop_id,
                                                                   stop_pax, bus_id, bus_type, bus_pax)
-            output_file.write(line)
+            output_file.write(line)  # writing initialize event
             ent += 1
 
             print('        Running ...')
@@ -103,8 +101,10 @@ class TransitLine:
 
                 # event type is passenger arrival
                 if isinstance(event_list[0], Stop):
-                    event_type = 3
-                    stop_id, stop_pax = event_list[0].run_pax_arrival()
+                    event_type = 3  # pax arrival event type
+                    event_list[0].run_pax_arrival()
+                    stop_id = event_list[0].stop_id
+                    stop_pax = event_list[0].num_pax
                     if save_stop:
                         bus_id, bus_type, bus_pax = '', '', ''
                         line = '{0},{1:.2f},{2},{3},{4},{5},{6},{7}\n'.format(ent, clk, event_type, stop_id,
@@ -115,6 +115,7 @@ class TransitLine:
 
                     # event type is bus arrival to stop
                     if event_list[0][1] == 'arrival':
+                        event_type = 1  # bus arrival event type
                         stop_id, stop_pax, bus_id, bus_type, bus_pax = event_list[0][0].run_arrival()
                         if save_bus:
                             line = '{0},{1:.2f},{2},{3},{4},{5},{6},{7}\n'.format(ent, clk, event_type, stop_id,
@@ -123,6 +124,7 @@ class TransitLine:
 
                     # event type is bus departure from stop
                     elif event_list[0][1] == 'departure':
+                        event_type = 2  # bus departure event type
                         stop_id, stop_pax, bus_id, bus_type, bus_pax = event_list[0][0].run_departure()
                         if save_bus:
                             line = '{0},{1:.2f},{2},{3},{4},{5},{6},{7}\n'.format(ent, clk, event_type, stop_id,
@@ -330,11 +332,14 @@ class Bus:
 
         # update stop idx
         self._stop_idx += 1
+
+        # remove alighting passengers from bus
         num_pax_off = len(self._pax_lists[self._stop_idx])
         self._num_pax -= num_pax_off
         self._pax_lists[self._stop_idx] = []
 
-        on_pax = stops[self._stop_idx]
+        # find passengers that want to board bus at this stop
+        on_pax = stops[self._stop_idx].pax
         num_pax_on = 0
         for pax in on_pax:
             if self._num_pax < self._bus_capacity:
@@ -344,7 +349,11 @@ class Bus:
                 self._pax_lists[pax.destination].append(pax)
             else:
                 break
+        # remove the boarding passengers from the stop
+        stops[self._stop_idx].run_pax_boarding(num_pax_on)
 
+        # schedule bus departure from stop based on time it will take for pax to board and alight
+        # TODO: TAKE CARE OF PAX THAT ARRIVE WHILE PAX ARE BOARDING
         self.schedule_departure(num_pax_off, num_pax_on)
 
     def run_departure(self):
@@ -382,6 +391,7 @@ class Stop:
 
         # the stop starts with no pax
         self._pax = []  # will hold instances of pax class
+        self._num_pax = 0
 
         # FOR DEBUG
         self.schedule_pax_arrival()
@@ -397,6 +407,10 @@ class Stop:
     @property
     def pax(self):
         return self._pax
+
+    @property
+    def num_pax(self):
+        return self._num_pax
 
     def schedule_pax_arrival(self):
         """
@@ -426,11 +440,22 @@ class Stop:
 
         # append to pax list
         self._pax.append(Pax(self._stop_id, destination))
+        self._num_pax += 1
 
         # schedule next passenger arrival
         self.schedule_pax_arrival()
 
-        return self._stop_id, len(self._pax)
+    def run_pax_boarding(self, num_pax_boarding):
+        """
+        this function removes a number of pax from the stop as they board the bus
+        :param num_pax_boarding: number of pax boarding a bus
+        :return:
+        """
+
+        # remove first num_pax_boarding pax from the pax list
+        del self._pax[0:num_pax_boarding]
+        self._num_pax -= num_pax_boarding
+
 
 
 class Pax:
